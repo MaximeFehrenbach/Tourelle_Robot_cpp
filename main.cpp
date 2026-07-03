a888d2781773b2c67ee50b3f6a60505a1cc423ad
Maxime <fehrenbach.max@gmail.com>
2026-07-03 19:57:17 +0200

    Test de connexion I2C a l'adresse 0x40

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <fcntl.h>      // open()
+#include <sys/ioctl.h>  // ioctl()
+#include <linux/i2c-dev.h> // I2C
+#include <unistd.h>     // close()
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello World" << std::endl;  
+    cout << "test" << endl;
+
+    // ouverture port I2C
+    int file = open("/dev/i2c-1", O_RDWR); // O_RDWR pour les droits de lecture et d'ecriture
+    
+    if (file < 0) {
+        cerr << "erreur I2C" << endl;
+        return 1;
+    }
+    cout << "I2C valide" << endl;
+
+    // on cible précisément la puce en hexa
+    int adresse_puce = 0x40;
+    
+    if (ioctl(file, I2C_SLAVE, adresse_puce) < 0) { //configuration du port
+        cerr << "erreur puce" << endl;
+        close(file);
+        return 1;
+    }
+    cout << "puce valide" << endl;
+
+    //fermeture du port
+    close(file);
+    cout << "fin test" << endl;
+    
     return 0;
 }
