ecc7d93262dfdf2c9aacd74a45780c208fece299
Maxime <fehrenbach.max@gmail.com>
2026-07-06 17:21:43 +0200

    Test séquence moteur

--- main.cpp
+++ main.cpp
@@ -17,8 +17,29 @@ void write_byte(int file, int register_address, int data) {
     write(file, buffer, 2);
 }
 
+void init_50Hz(int file){
+    // mise en veille
+    write_byte(file, 0x00, 0x10); 
+    // fréquence à 50 Hz
+    write_byte(file, 0xFE, 121); // 121 = 25MHz/(2^12*50Hz)-1
+    // réveil du PCA
+    write_byte(file, 0x00, 0x20);
+    // on laisse l'horloge du PCA se stabiliser
+    this_thread::sleep_for(chrono::milliseconds(5));
+    cout << "config à 50 Hz" << endl;
+}
+
+// faire tourner un moteur
+void set_servo(int file, int canal, int off_value) {
+    int servo_address = 0x06 + (4 * canal); // adresse de départ du canal
+    write_byte(file, servo_address, 0); // ON_L
+    write_byte(file, servo_address + 1, 0); // ON_H
+    write_byte(file, servo_address + 2, off_value & 0xFF); // OFF_L
+    write_byte(file, servo_address + 3, off_value >> 8); // OFF_H
+}
+
 int main() {
-    cout << "test mouvement" << endl;
+    cout << "test séquence de mouvements" << endl;
 
     // ouverture port I2C
     int file = open("/dev/i2c-1", O_RDWR); // O_RDWR pour les droits de lecture et d'ecriture
@@ -37,36 +58,16 @@ int main() {
         return 1;
     }
     
-    // mise en veille
-    write_byte(file, 0x00, 0x10); 
-    // fréquence à 50 Hz
-    write_byte(file, 0xFE, 121); // 121 = 25MHz/(2^12*50Hz)-1
-    // réveil du PCA
-    write_byte(file, 0x00, 0x20);
-    // on laisse l'horloge du PCA se stabiliser
-    this_thread::sleep_for(chrono::milliseconds(5));
-    cout << "config à 50 Hz" << endl;
+    init_50Hz(file);
 
-    cout << "moteur a 90°" << endl;
+    set_servo(file, 0, 300); 
+    this_thread::sleep_for(chrono::seconds(2));
 
-    // Adresses des registres
-    int LED0_ON_L  = 0x06;
-    int LED0_ON_H  = 0x07;
-    int LED0_OFF_L = 0x08;
-    int LED0_OFF_H = 0x09;
+    set_servo(file, 0, 420); 
+    this_thread::sleep_for(chrono::seconds(2));
 
-    // Nos valeurs cibles
-    int on_value = 0;     // tick 0
-    int off_value = 360;  // tick 307 = (1.5ms / 20ms)* 2^12
+    set_servo(file, 0, 0); 
 
-    // ecriture dans les registre
-    write_byte(file, LED0_ON_L, on_value & 0xFF);
-    write_byte(file, LED0_ON_H, on_value >> 8);
-    
-    write_byte(file, LED0_OFF_L, off_value & 0xFF);
-    write_byte(file, LED0_OFF_H, off_value >> 8);
-
-    cout << "ordre moteur envoyé" << endl;
     close(file);
     return 0;
 }
